68f7f59f26d758e7ff202f66f446344faf6a2518
EkuDeku <ekuman.k@gmail.com>
2019-12-16 23:47:43 +0100

    Anim and Mesh Import Override test

---
+++ Assets/Editor/MeshImportOverwrite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MeshImportOverwrite : AssetPostprocessor
+{
+
+    private void OnPreprocessModel()
+    {
+
+        ModelImporter importer = (ModelImporter)assetImporter;
+        importer.materialImportMode = ModelImporterMaterialImportMode.None;
+        importer.isReadable = true;
+
+    }
+
+}
